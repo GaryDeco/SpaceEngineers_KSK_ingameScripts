8acc3bc4ff1d0ffffa54aeaf3477d953682725d5
Gary <35435241+DekozmicMayhem@users.noreply.github.com>
2019-03-27 16:08:31 -0400

    updated readme

--- SE_Template_HelloWorld/SE_Template_HelloWorld/Readme.cs
+++ SE_Template_HelloWorld/SE_Template_HelloWorld/Readme.cs
@@ -2,7 +2,16 @@
  *   R e a d m e
  *   -----------
  * 
- *   In this file you can include any instructions or other comments you want to have injected onto the 
- *   top of your final script. You can safely delete this file if you do not want any such comments.
+ * This script is a simple print "Hello world" to a text panel on your grid. I specified "LCD1" in searching for 
+ * the text panel. So it should be named LCD1 if running the code as is. The code will do exactly as specified so if there were 2 LCD's 
+ * named "LCD1" it would display the text on both of them, so keep that in mind. The search method used will 
+ * return any text panels containing that specific text. This gets the job done but you may want a better 
+ * method for calling a specific block. You can create tags which I will do in a later script. Reading the comments should give a
+ * clear idea of the logical process that I followed. If you think about it, we do this all the time when accomplishing a task.
+ *
+ * Note: If you are not using visual studio, just copy / paste the code within the void main block directly into the programmable
+ * block. Be sure to include all brackets. 
  * 
+ *
+ *
  */
